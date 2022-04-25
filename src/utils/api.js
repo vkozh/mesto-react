fa207231e237aa861b936d3d9b2ab74fbfbb3d33
Lekozhe <v.a.kozh@ya.ru>
2022-04-25 16:56:47 +0300

    feat: api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,75 +1,93 @@
-export default class Api {
-    constructor({
-        baseUrl,
-        headers
-    }) {
-        this._url = baseUrl;
-        this._headers = headers;
-    }
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._url = baseUrl;
+    this._headers = headers;
+  }
 
-    _fetch(path, method, bodyObject, form) {
-        if (bodyObject === undefined) {
-            return fetch(`${this._url}${path}`, {
-                    method: method,
-                    headers: this._headers
-                })
-                .then(this._checkResponse);
-        } else {
-            form.renderLoading(true);            
-            return fetch(`${this._url}${path}`, {
-                    method: method,
-                    headers: this._headers,
-                    body: JSON.stringify(bodyObject)
-                })
-                .then(this._checkResponse)
-                .finally(() => form.renderLoading(false));
-        }
+  _fetch(path, method, bodyObject, form) {
+    if (bodyObject === undefined) {
+      return fetch(`${this._url}${path}`, {
+        method: method,
+        headers: this._headers,
+      }).then(this._checkResponse);
+    } else {
+      form.renderLoading(true);
+      return fetch(`${this._url}${path}`, {
+        method: method,
+        headers: this._headers,
+        body: JSON.stringify(bodyObject),
+      })
+        .then(this._checkResponse)
+        .finally(() => form.renderLoading(false));
     }
+  }
 
-    _checkResponse(res) {
-        if (res.ok)
-            return res.json();
-        return Promise.reject(`Ошибка ${res.status}`);
-    }
+  _checkResponse(res) {
+    if (res.ok) return res.json();
+    return Promise.reject(`Ошибка ${res.status}`);
+  }
 
+  getUser() {
+    return this._fetch("/users/me", "GET");
+  }
 
-    getUser() {
-        return this._fetch('/users/me', 'GET');
-    }
+  getCards() {
+    return this._fetch("/cards", "GET");
+  }
 
-    getCards() {
-        return this._fetch('/cards', 'GET');
-    }
+  editProfile(name, about, button) {
+    return this._fetch(
+      "/users/me",
+      "PATCH",
+      {
+        name: name,
+        about: about,
+      },
+      button
+    );
+  }
 
-    editProfile(name, about, button) {
-        return this._fetch('/users/me', 'PATCH', {
-            name: name,
-            about: about
-        }, button);
-    }
+  addCard(name, link, button) {
+    return this._fetch(
+      "/cards",
+      "POST",
+      {
+        name: name,
+        link: link,
+      },
+      button
+    );
+  }
 
-    addCard(name, link, button) {
-        return this._fetch('/cards', 'POST', {
-            name: name,
-            link: link
-        }, button);
-    }
+  deleteCard(cardId) {
+    return this._fetch(`/cards/${cardId}`, "DELETE");
+  }
 
-    deleteCard(cardId) {
-        return this._fetch(`/cards/${cardId}`, 'DELETE');
-    }
+  setLike(cardId) {
+    return this._fetch(`/cards/${cardId}/likes`, "PUT");
+  }
 
-    setLike(cardId) {
-        return this._fetch(`/cards/${cardId}/likes`, 'PUT');
-    }
+  deleteLike(cardId) {
+    return this._fetch(`/cards/${cardId}/likes`, "DELETE");
+  }
 
-    deleteLike(cardId) {
-        return this._fetch(`/cards/${cardId}/likes`, 'DELETE');
-    }
-
-    changeAvatar(avatar, form) {
-        return this._fetch('/users/me/avatar', 'PATCH', {
-            avatar: avatar
-        }, form);
-    }
+  changeAvatar(avatar, form) {
+    return this._fetch(
+      "/users/me/avatar",
+      "PATCH",
+      {
+        avatar: avatar,
+      },
+      form
+    );
+  }
 }
+
+const api = new Api({
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-39",
+  headers: {
+    authorization: "d0163cf8-cfab-4a34-ac21-cc13d220d7ff",
+    "Content-Type": "application/json",
+  },
+});
+export default api;
